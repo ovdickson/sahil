6152085e2f4614789bdb1fd95fe22229d3d32a0b
ovdickson <dickson@cloudplexo.com>
2024-09-30 09:51:13 +0100

    images workflow

--- infra/terraform/ecs-ec2/logs.tf
+++
@@ -1,7 +0,0 @@
-# resource "aws_cloudwatch_log_group" "sahil_log_grp" {
-#   name = "${var.cust_name}-cluster-log"
-# }
-
-resource "aws_cloudwatch_log_group" "sahil_cluster_log_grp" {
-  name = "${var.cust_name}-cluster-log-${var.env}"
-}
--- infra/terraform/networking/igw.tf
+++
@@ -1,6 +0,0 @@
-
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = merge(local.tags, tomap({ "Name" = "${var.cust_name}-igw" }))
-}
