ad276f08b41803cb0dd88b01f8270fd971ea71c5
ovdickson <dickson@cloudplexo.com>
2024-10-06 01:02:57 +0100

    images workflow

--- infra/terraform/versions.tf
+++ infra/terraform/versions.tf
@@ -1,3 +1,10 @@
+provider "aws" {
+  region  = "eu-west-1"
+  assume_role {
+    role_arn = "arn:aws:iam::${var.aws_account_id}:role/${var.iam_name}"
+  }
+}
+
 terraform {
   required_version = "~> 1"
 
@@ -8,5 +15,12 @@ terraform {
     }
   }
 
-  backend "s3" {}
+  backend "s3" {
+    encrypt = true
+
+    backend_s3_bucket         = "sahil-terraform-state-bucket" 
+    backend_dynamodb_table    = "sahil-terraform-table-locks"   
+    backend_iam_role          = "sahil-deployment-role"  
+    aws_region         = "eu-west-1"
+  }
 }
